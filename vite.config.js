4e3461cb99cb81b42205aa041a74cc94dabcf3b9
Dawa <atayewdawlet@gmail.com>
2024-04-30 13:47:56 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://dendyla.github.io/gameRun/"
+  base: "https://github.com/DendyLA/gameRun"
 })
+
+// https://dendyla.github.io/gameRun/
